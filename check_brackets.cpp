f47f55de35fac98289fe20aa06e954afb757b8f9
uppuluriaditya <uppuluriaditya@gmail.com>
2017-02-05 17:30:38 +0530

    Update check_brackets.cpp

--- check_brackets.cpp
+++ check_brackets.cpp
@@ -31,39 +31,39 @@ int main() {
     for (int position = 0; position < text.length(); ++position) {
         char next = text[position];
         if (next == '(' || next == '[' || next == '{') {
-			opening_brackets_stack.push(Bracket(next, position));
+		opening_brackets_stack.push(Bracket(next, position));
         }
         if (next == ')' || next == ']' || next == '}') {
-			if(opening_brackets_stack.empty()){
-				unmatchedPosition = position+1;
-				break;
+		if(opening_brackets_stack.empty()){
+			unmatchedPosition = position+1;
+			break;
+		}
+		else
+		{
+			if(opening_brackets_stack.top().Matchc(next)){
+				opening_brackets_stack.pop();
 			}
 			else
 			{
-				if(opening_brackets_stack.top().Matchc(next)){
-					opening_brackets_stack.pop();
-				}
-				else
-				{
-					isCloseBracket = true;
-					unmatchedPosition = position+1;
-					break;
-				}
+				isCloseBracket = true;
+				unmatchedPosition = position+1;
+				break;
 			}
+		}
         }
     }
-	if(!opening_brackets_stack.empty()){
-		if(isCloseBracket)
-			std::cout<<unmatchedPosition<<std::endl;
-		else
-			std::cout<<opening_brackets_stack.top().position+1<<std::endl;
-	}
-	else{
+    if(!opening_brackets_stack.empty()){
+	if(isCloseBracket)
+		std::cout<<unmatchedPosition<<std::endl;
+	else
+		std::cout<<opening_brackets_stack.top().position+1<<std::endl;
+    }
+    else{
 		if(unmatchedPosition == 0)
 			std::cout<<"Success"<<std::endl;
 		else
 			std::cout<<unmatchedPosition<<std::endl;
-	}
+    }
 		
     return 0;
 }
